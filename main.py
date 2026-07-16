044cebc06b31813da2c86ce2cde7490770980717
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-07-16 09:18:36 +0500

    Use environment variables for PostgreSQL connection

--- main.py
+++ main.py
@@ -1,179 +1,233 @@
-from fastapi import Body, FastAPI
-from fastapi import status 
+from fastapi import FastAPI
+from fastapi import status
 from fastapi import HTTPException
 from pydantic import BaseModel
+import time
+import os
+from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import DictCursor
 
 app = FastAPI()
 
+load_dotenv()
 
+while True:
+    try:
+        con = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            cursor_factory=DictCursor
+        )
+
+        cursor = con.cursor()
+        print("Connected to database successfully")
+        break
+
+    except Exception as error:
+        print("Failed to connect to database")
+        print("Error:", error)
+        time.sleep(2)
 # -----------------------------
 # Movie Management System
 # -----------------------------
 
-movie_store = []
-
 
 class Movie(BaseModel):
-    movie_id: str
     title: str
     director: str
     genre: str
     release_year: int
-    rating: float
-    available: bool
+    rating: int
+    is_availible: bool
 
 
 @app.get("/")
 def home():
-    return {
-        "message": "Welcome to Movie Management System"
-    }
+    return {"message": "Welcome to Movie Management System"}
 
 
 # -----------------------------
 # GET ALL MOVIES
 # -----------------------------
-@app.get("/movies",status_code = status.HTTP_200_OK)
+
+@app.get("/movies/count", status_code=status.HTTP_200_OK)
+def total_movies():
+
+    cursor.execute("SELECT COUNT(*) AS total FROM movies")
+
+    total = cursor.fetchone()
+
+    return {"total_movies": total["total"]}
+
+@app.get("/movies", status_code=status.HTTP_200_OK)
 def get_movies():
-    return {
-        "message": "All Movies",
-        "data": movie_store
-    }
+    cursor.execute("select * from movies")
+    movie = cursor.fetchall()
+    return {"message": "All Movies", "data": movie}
 
 
 # -----------------------------
 # GET MOVIE BY ID
 # -----------------------------
-@app.get("/movies/{movie_id}", status_code= status.HTTP_200_OK)
-def get_movie_by_id(movie_id: str):
-    for movie in movie_store:
-        if movie["movie_id"] == movie_id:
-            return {
-                "message": "Movie Found",
-                "data": movie
-            }
+@app.get("/movies/{id}", status_code=status.HTTP_200_OK)
+def get_movie_by_id(id: int):
+    cursor.execute("SELECT * FROM movies WHERE movie_id = %s", (id,))
 
-    raise HTTPException(
-        status_code = status.HTTP_404_NOT_FOUND,
-        detail = "Movies Not Found "
-    )
+    movie = cursor.fetchone()
+
+    if movie is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Movie Not Found"
+        )
+
+    return {"message": "Movie Found", "data": movie}
 
 
 # -----------------------------
 # GET MOVIES BY GENRE
 # -----------------------------
-@app.get("/movies/genre/{genre}", status_code = status.HTTP_200_OK)
-def get_movies_by_genre(genre: str):
-    movies = []
+@app.get("/movies/genre/{gen}", status_code=status.HTTP_200_OK)
+def get_movies_by_genre(gen: str):
 
-    for movie in movie_store:
-        if movie["genre"].lower() == genre.lower():
-            movies.append(movie)
+    cursor.execute("select * from movies where genre = %s", (gen,))
+    movies = cursor.fetchall()
+    if not movies:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Movies Not Found"
+        )
 
-    if movies:
-        return {
-            "message": "Movies Found",
-            "data": movies
-        }
-    raise HTTPException(
-        status_code= status.HTTP_404_NOT_FOUND,
-        detail = "Movies Not Found"
-    )
+    return {"message": "Movies Found", "data": movies}
 
 
 # -----------------------------
 # GET AVAILABLE MOVIES
 # -----------------------------
-@app.get("/movies/available/{available}", status_code= status.HTTP_200_OK)
+@app.get("/movies/is_availible/{available}", status_code=status.HTTP_200_OK)
 def get_available_movies(available: bool):
-    movies = []
 
-    for movie in movie_store:
-        if movie["available"] == available:
-            movies.append(movie)
+    cursor.execute("select * from movies where is_availible = %s", (available,))
+    movies = cursor.fetchall()
 
-    return {
-        "message": "Available Movies",
-        "data": movies
-    }
+    if not movies:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Movies not Found"
+        )
+
+    return {"message": "Available Movies", "data": movies}
 
 
 # -----------------------------
 # GET MOVIES BY MINIMUM RATING
 # -----------------------------
-@app.get("/movies/rating/{rating}", status_code = status.HTTP_200_OK)
-def get_movies_by_rating(rating: float):
-    movies = []
+@app.get("/movies/rating/{rate}", status_code=status.HTTP_200_OK)
+def get_movies_by_rating(rate: int):
 
-    for movie in movie_store:
-        if movie["rating"] >= rating:
-            movies.append(movie)
+    cursor.execute("select * from movies where rating > %s", (rate,))
+    movies = cursor.fetchall()
 
-    return {
-        "message": "Movies Found",
-        "data": movies
-    }
+    if not movies:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Movies Not Found above this rating",
+        )
+    return {"message": "Movies Found", "data": movies}
 
-
-# -----------------------------
-# GET TOTAL MOVIES
-# -----------------------------
-@app.get("/movies/count" , status_code= status.HTTP_200_OK)
-def total_movies():
-    return {
-        "total_movies": len(movie_store)
-    }
 
 
 # -----------------------------
 # CREATE MOVIE
 # -----------------------------
-@app.post("/movies", status_code = status.HTTP_200_OK)
+@app.post("/movies", status_code=status.HTTP_200_OK)
 def create_movie(movie: Movie):
-    movie_store.append(movie.model_dump())
+    cursor.execute(
+        """INSERT INTO movies
+        (title, director, genre, release_year, rating, is_availible)
+        VALUES (%s,%s,%s,%s,%s,%s)
+        RETURNING *;
+        """,
+        (
+            movie.title,
+            movie.director,
+            movie.genre,
+            movie.release_year,
+            movie.rating,
+            movie.is_availible,
+        ),
+    )
 
-    return {
-        "message": "Movie added successfully",
-        "data": movie
-    }
+    new_movie = cursor.fetchone()
+    con.commit()
+
+    return {"message": "Movie added successfully", "data": new_movie}
 
 
 # -----------------------------
 # UPDATE MOVIE
 # -----------------------------
-@app.put("/movies/{movie_id}" ,status_code =  status.HTTP_200_OK)
-def update_movie(movie_id: str, updated_movie: Movie):
+@app.put("/movies/{id}", status_code=status.HTTP_200_OK)
+def update_movie(id: int, updated_movie: Movie):
 
-    for movie in movie_store:
-        if movie["movie_id"] == movie_id:
-            movie.update(updated_movie.model_dump())
-
-            return {
-                "message": "Movie updated successfully",
-                "data": movie
-            }
-
-    raise HTTPException(
-        status_code= status.HTTP_404_NOT_FOUND,
-        detail = "Movie Not Found"
+    cursor.execute(
+        """
+        UPDATE movies
+        SET
+            title = %s,
+            director = %s,
+            genre = %s,
+            release_year = %s,
+            rating = %s,
+            is_availible = %s
+        WHERE movie_id = %s
+        RETURNING *;
+    """,
+        (
+            updated_movie.title,
+            updated_movie.director,
+            updated_movie.genre,
+            updated_movie.release_year,
+            updated_movie.rating,
+            updated_movie.is_availible,
+            id,
+        ),
     )
+
+    movie = cursor.fetchone()
+
+    if movie is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Movie Not Found"
+        )
+
+    con.commit()
+
+    return {"message": "Movie Updated Successfully", "data": movie}
+
 
 # -----------------------------
 # DELETE MOVIE
 # -----------------------------
-@app.delete("/movies/{movie_id}")
-def delete_movie(movie_id: str):
+@app.delete("/movies/{id}", status_code=status.HTTP_200_OK)
+def delete_movie(id: int):
 
-    for movie in movie_store:
-        if movie["movie_id"] == movie_id:
-            movie_store.remove(movie)
-
-            return {
-                "message": "Movie deleted successfully",
-                "data": movie
-            }
-
-    raise HTTPException(
-        status_code = status.HTTP_404_NOT_FOUND,
-        detail = "Movie not found"
+    cursor.execute(
+        """
+        DELETE FROM movies
+        WHERE movie_id = %s
+        RETURNING *;
+    """,
+        (id,),
     )
+
+    movie = cursor.fetchone()
+
+    if movie is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Movie Not Found"
+        )
+
+    con.commit()
+
+    return {"message": "Movie Deleted Successfully", "data": movie}
